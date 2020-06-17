b750c7bc7d19b5ce847956c277205f368a144f5f
Daaarii <zuganich48@yandex.ru>
2020-06-17 16:55:55 +0300

    Updates

---
+++ precache-manifest.d65423fedbf2bc2a3c7b5fa550097570.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219bbc3c3287407b0d3fa45d077a894a",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "e00ed0d134cf1abf89e7",
+    "url": "/kanban-board/static/css/main.2102069d.chunk.css"
+  },
+  {
+    "revision": "e0db9922525e7080a98c",
+    "url": "/kanban-board/static/js/2.c7d390b3.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/kanban-board/static/js/2.c7d390b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00ed0d134cf1abf89e7",
+    "url": "/kanban-board/static/js/main.9fe581d5.chunk.js"
+  },
+  {
+    "revision": "8e241a090fc38b565af0",
+    "url": "/kanban-board/static/js/runtime-main.b82108f8.js"
+  },
+  {
+    "revision": "6cb6e5fc57e9600d8f9f7cdfaa120d3d",
+    "url": "/kanban-board/static/media/addBtn.6cb6e5fc.svg"
+  },
+  {
+    "revision": "0adf851bce82cc59f5438658221e0a6b",
+    "url": "/kanban-board/static/media/background.0adf851b.png"
+  },
+  {
+    "revision": "5fe0db661a8f134c8ff7cb418373c469",
+    "url": "/kanban-board/static/media/cross.5fe0db66.svg"
+  }
+]);
